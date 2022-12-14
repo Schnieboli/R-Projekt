e126668f164c8a52f0da411eec0d7f62ca8dfd7f
Rodi79 <119407474+Rodi79@users.noreply.github.com>
2022-12-14 11:02:37 +0100

    Create Funktion1.R

---
+++ Funktion1.R
@@ -0,0 +1,11 @@
+fun1 <- function(x){
+  a <- mean(x)
+  b <- median(x)
+  c <- min(x)
+  d <- max(x)
+  cat("Das arithmetische Mittel ist", a, "und der Median ist", b,
+      ". Das Minimum des Vektors ist", c, " und das Maximum ist", d,
+      ", also ist die Range des Vektors", d-c)
+}
+fun1(x)
+x <- c(1,2,3)
