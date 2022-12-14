16319bf47d4ab5dad068410846e5bf5f07db632e
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2022-12-14 16:00:46 +0100

    Update Funkt5.R
    hoffentlich ist der code jetzt auch da

---
+++ Funkt5.R
@@ -0,0 +1,10 @@
+# Funktion e) aus dem zweiten Skript
+
+Funktion5 <- function(x){
+  # Obere und untere Grenze werden festgelegt -> unteres und oberes Quartil
+  G_u <- quantile(x, 0.25)
+  G_o <- quantile(x, 0.75)
+  # dann wird der Vektor an den Quartilen aufgeteilt, bis unteres Quartil "niedrig"
+  # dann "mittel" und ab oberen Quartil "hoch"
+  cut(x, breaks = c(min(x)-1, G_u, G_o, max(x)+1), labels = c("niedig","mittel","hoch"))
+}
