d4724e25e9ef8678e0679165fe249366b7e0da54
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2022-12-14 18:20:00 +0100

    Update Funktion4.R
    Die Funktion hat doch gar nicht funktioniert oder? Wenn Datensatz nicht Datensatz heißen würde, wüsste die Funktion gar nicht, was sie machen sollte...
    Und der Funktionsaufruf gehört nicht zur Funktion, deswegen würde ich den rausmachen.

--- Funktion4.R
+++ Funktion4.R
@@ -1,14 +1,13 @@
 fun4 <- function(x){
-  a <- mean(Datensatz$Int_Mathe[Datensatz$MatheLK == "ja"])
+  a <- mean(x$Int_Mathe[Datensatz$MatheLK == "ja"])
   # Durchschnittsinteresse für Mathematik, wenn sie Mathe-LK hatten.
-  b <- mean(Datensatz$Int_Programmieren[Datensatz$MatheLK == "ja"])
+  b <- mean(x$Int_Programmieren[Datensatz$MatheLK == "ja"])
   # Durchschnittsinteresse für Programmieren, wenn sie Mathe-LK hatten.
-  c <- mean(Datensatz$Int_Mathe[Datensatz$MatheLK == "nein"])
+  c <- mean(x$Int_Mathe[Datensatz$MatheLK == "nein"])
   # Durchschnittsinteresse für Mathematik, wenn sie kein Mathe-LK hatten.
-  d <- mean(Datensatz$Int_Programmieren[Datensatz$MatheLK == "nein"])
+  d <- mean(x$Int_Programmieren[Datensatz$MatheLK == "nein"])
   # Durchschnittsinteresse für Programmieren, wenn sie kein Mathe-LK hatten.
   cat("Das Durchschnittsinteresse an Mathematik ist ", round(a, 3), " und an Programmieren ", round(b, 3),
       ", wenn\ndie Person Mathe-LK hatte.\nDas Durchschnittsinteresse an Mathematik ist ", round(c, 3), " und an Programmieren ist ", round(d,3),
       ", wenn\ndie Person kein Mathe-LK hatte.", sep = "")
 }
-fun4(Datensatz)
