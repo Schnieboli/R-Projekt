0923ba11316c810ecb283187e596df2023de502c
NicoleHofmann <119400639+NicoleHofmann@users.noreply.github.com>
2022-12-15 12:49:07 +0100

    Update Funkt5.R

--- Funkt5.R
+++ Funkt5.R
@@ -6,5 +6,5 @@ Funktion5 <- function(x){
   G_o <- quantile(x, 0.75)
   # dann wird der Vektor an den Quartilen aufgeteilt, bis unteres Quartil "niedrig"
   # dann "mittel" und ab oberen Quartil "hoch"
-  cut(x, breaks = c(min(x)-1, G_u, G_o, max(x)+1), labels = c("niedig","mittel","hoch"))
+  cut(x, breaks = c(min(x)-1, G_u, G_o, max(x)+1), labels = c("niedrig","mittel","hoch"))
 }
