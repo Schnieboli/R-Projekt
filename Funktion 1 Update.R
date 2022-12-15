2a555f28e72c2d28f966365f268fa89c7f8ccb4e
maxkuebler <max@mikekuebler.de>
2022-12-15 12:52:46 +0100

    Create Funktion 1 Update.R
    Standardabweichung hinzugefügt. Ausgabe als Aufzählung. Komma hinzugefügt.

---
+++ Funktion 1 Update.R
@@ -0,0 +1,8 @@
+fun1 <- function(x){
+  a <- mean(x)
+  b <- median(x)
+  c <- min(x)
+  d <- max(x)
+  e <- sd(x)
+  cat("Das arithmetische Mittel ist ", a, ".\nDer Median ist ", b, ".\nDie Standardabweichung ist ",e,".\nDas Minimum des Vektors ist ", c, ". \nDas Maximum des Vektors ist ", d,". \nDie Range des Vektors ist ", d-c, ".", sep = "")
+}
