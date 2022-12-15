eb2a439802f18ced5e6653bda655b88d70b80e3a
Rodi79 <119407474+Rodi79@users.noreply.github.com>
2022-12-15 13:09:17 +0100

    Update Funktion4.R

--- Funktion4.R
+++ Funktion4.R
@@ -1,11 +1,11 @@
 fun4 <- function(x){
-  a <- round(mean(x$Interesse.an.Mathematik[x$Mathe.LK == "Ja"]),3)
+  a <- round(mean(x$Int_Mathe[x$MatheLK == "ja"]),3)
   # Durchschnittsinteresse für Mathematik, wenn sie Mathe-LK hatten.
-  b <- round(mean(x$Interesse.an.Programmieren[x$Mathe.LK == "Ja"]),3)
+  b <- round(mean(x$Int_Programmieren[x$MatheLK == "ja"]),3)
   # Durchschnittsinteresse für Programmieren, wenn sie Mathe-LK hatten.
-  c <- round(mean(x$Interesse.an.Mathematik[x$Mathe.LK == "Nein"]),3)
+  c <- round(mean(x$Int_Mathe[x$MatheLK == "nein"]),3)
   # Durchschnittsinteresse für Mathematik, wenn sie kein Mathe-LK hatten.
-  d <- round(mean(x$Interesse.an.Programmieren[x$Mathe.LK == "Nein"]),3)
+  d <- round(mean(x$Int_Programmieren[x$MatheLK == "nein"]),3)
   # Durchschnittsinteresse für Programmieren, wenn sie kein Mathe-LK hatten.
   cat("Mit Mathe-LK:", "\nDurchschnittsinteresse Mathe:", a, "\nDurchschnittsinteresse Programmieren:", b,
       "\nOhne Mathe-LK:", "\nDurchschnittsinteresse Mathe:", c, "\nDurchschnittsinteresse Programmieren:", d)
