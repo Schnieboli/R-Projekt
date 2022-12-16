31822aff3d52d3ddfdea51191447c3671450d163
maxkuebler <120549425+maxkuebler@users.noreply.github.com>
2022-12-16 16:12:03 +0100

    Update Funktion2.R
    Datensatz$... zu x$.. geändert, ausgabe von MatheLK == nein hinzugefügt

--- Funktion2.R
+++ Funktion2.R
@@ -2,22 +2,22 @@
 
 fun2 <- function(x) {
   
-  a<- sum(Datensatz$Studienfach == "Statistik") #Anteil der Studenten im Bereich Statistik
-  b<- sum(Datensatz$Studienfach == "Data Science") #Anteil der Studenten im Bereich Data Science
-  c<- sum(Datensatz$Studienfach == "Mathe") #Anteil der Studenten im Bereich Mathe
-  d<- sum(Datensatz$Studienfach == "Informatik") #Anteil der Studenten im Bereich Informatik
+  a<- sum(x$Studienfach == "Statistik") #Anteil der Studenten im Bereich Statistik
+  b<- sum(x$Studienfach == "Data Science") #Anteil der Studenten im Bereich Data Science
+  c<- sum(x$Studienfach == "Mathe") #Anteil der Studenten im Bereich Mathe
+  d<- sum(x$Studienfach == "Informatik") #Anteil der Studenten im Bereich Informatik
         
   
-  e<- sum (Datensatz$MatheLK == "ja") # Anteil der Studenten, die vorher einen MatheLK belegt hatten
-  f<- sum(Datensatz$MatheLK == "nein")
+  e<- sum (x$MatheLK == "ja") # Anteil der Studenten, die vorher einen MatheLK belegt hatten
+  f<- sum(x$MatheLK == "nein")# Anteil der Studenten, die vorher keinen MatheLK belegt hatten
   
-  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Statistik      " ,a)
-  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Data Science   " ,b)
-  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Mathe          ", c)
-  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Informatik     ", d)
+  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Statistik       " ,a)
+  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Data Science    " ,b)
+  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Mathe           ", c)
+  cat("\n" ,"Anzahl der Studierenden in dem Studienfach Informatik      ", d)
   cat("\n")
-  cat("\n", "Anzahl der Studierenden, die einen Mathe LK belegt hatten ", e)
-  
+  cat("\n", "Anzahl der Studierenden, die einen Mathe LK belegt hatten  ", e)
+  cat("\n", "Anzahl der Studierenden, die keinen Mathe LK belegt hatten ", f)
   # Ausgabe einer Übersicht der einzelnen Anteile an der Grundgesamtheit
 }
 
