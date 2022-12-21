3c8a076128e23a52a34b6291c0c4b2cc8c69138e
maxkuebler <120549425+maxkuebler@users.noreply.github.com>
2022-12-21 21:50:22 +0100

    Update Auswertung.R

--- Auswertung.R
+++ Auswertung.R
@@ -11,6 +11,16 @@ fun1(Datensatz$Alter)
 # Das Maximum des Vektors ist 33. 
 # Die Range des Vektors ist 18.
 
+## Das artithmetische Mittel (24.38) liegt knapp unter dem Median (25). Das Alter ist mit einer Standardabweichung von ca. 4 weder eng beisammen, 
+## noch weit gestreut. Die Jüngste Person ist 15 Jahre als, was vermutlich durch eine symmterische Verteilung verursacht wurde da, die älteste Person 33 Jahre alt ist.
+## Somit liegen zwischen dem Durchschnitt (gerundet auf 24), genau 9 Jahre zu dem Maximum/Minimum.
+
+fun6(Datensatz$Alter)
+
+## Man erkennt im Barplot eine fast symmetrische Häufigkeit um den Wert 25,
+## was auf eine Normalverteilung mit Mittelwert 25 mit zugehöriger Standardabweichung von ca. 4 schließen lässt.
+
+
 
 ## Analyse des Studienfaches/Mathe-LKs
 
@@ -30,35 +40,65 @@ fun2(Datensatz)
 ## Die Aufteilung des Merkmals des Mathe-LKs ist ungefähr 85% ja, 15% nein.
 
 
-##Analyse des Zusammenhangs zwischen Studienfach und Mathe-LK
+## Analyse des Zusammenhangs zwischen Studienfach und Mathe-LK
 
 fun3(Datensatz)
 
- #Der Anteil der Statistik Studierenden die den Mathe-LK belegt haben beträgt:     84.61538 %
- #Der Anteil der Data Science Studierenden die den Mathe-LK belegt haben beträgt:  79.31034 %
- #Der Anteil der Mathematik Studierenden die den Mathe-LK belegt haben beträgt:    92.85714 %
- #Der Anteil der Informatik Studierenden die den Mathe-LK belegt haben beträgt:    90.32258 %
+ # Der Anteil der Statistik Studierenden die den Mathe-LK belegt haben beträgt:     84.61538 %
+ # Der Anteil der Data Science Studierenden die den Mathe-LK belegt haben beträgt:  79.31034 %
+ # Der Anteil der Mathematik Studierenden die den Mathe-LK belegt haben beträgt:    92.85714 %
+ # Der Anteil der Informatik Studierenden die den Mathe-LK belegt haben beträgt:    90.32258 %
 
-##Der höchste Anteil der Studierenden, die in der Schule Mathe-LK belegt hatten, finden wir im Studiengang der Mathematik mit 93%.
-##Weiter ist der Anteil von 90 % Mathe-LK Teilnehmer im Studiengang Informatik, 85% der Studierenden des Studiengangs Statistik und 
-##80% der Studierenden im Studiengang Data Science das Ergebnis der Auswertung.
+## Der höchste Anteil der Studierenden, die in der Schule Mathe-LK belegt hatten, finden wir im Studiengang der Mathematik wieder, mit 93%.
+## Weiter ist der Anteil von Mathe-LK Teilnehmern im Studiengang Informatik 90 %, im Studiengang Statistik 85% und 
+## im Studiengang Data Science 80%.
 
 
-##Analyse der Zusammenhänge zwischen "Interesse an Mathematik" und Mathe-LK und "Interesse am Programmieren" und Mathe-LK
+## Analyse von Interessen an Mathematik und Programmieren
+
+fun1(Datensatz$Int_Mathe)
+
+# Das arithmetische Mittel ist 5.32.
+# Der Median ist 5.5.
+# Die Standardabweichung ist 1.254326.
+# Das Minimum des Vektors ist 2. 
+# Das Maximum des Vektors ist 7. 
+# Die Range des Vektors ist 5.
+
+## Das Durchschnittliche Interesse an Mathematik liegt bei 5.32 . Von einer Skala von 1 (sehr gering) bis 7 (sehr hoch) sind alle Werte außer 1 vertreten.
+
+fun1(Datensatz$Int_Programmieren)
+
+# Das arithmetische Mittel ist 5.61.
+# Der Median ist 6.
+# Die Standardabweichung ist 1.213518.
+# Das Minimum des Vektors ist 2. 
+# Das Maximum des Vektors ist 7. 
+# Die Range des Vektors ist 5.
+
+## Das Durchschnittliche Interesse an Informatik liegt bei 5.32 . Von einer Skala von 1 (sehr gering) bis 7 (sehr hoch) sind alle Werte außer 1 vertreten.
+## Auch sieht man das im Durchschnitt das Interesse an Informatik (ca. 5.6) etwas höher ist als das Interesse an Mathematik (ca. 5.3).
+
+
+
+## Analyse der Zusammenhänge zwischen "Interesse an Mathematik" und Mathe-LK und "Interesse am Programmieren" und Mathe-LK
 
 fun4(Datensatz)
 
-#Mit Mathe-LK: 
-#Durchschnittsinteresse Mathe: 5.267 
-#Durchschnittsinteresse Programmieren: 5.605 
-#Ohne Mathe-LK: 
-#Durchschnittsinteresse Mathe: 5.643 
-#Durchschnittsinteresse Programmieren: 5.643
+# Mit Mathe-LK: 
+# Durchschnittsinteresse Mathe: 5.267 
+# Durchschnittsinteresse Programmieren: 5.605 
+# Ohne Mathe-LK: 
+# Durchschnittsinteresse Mathe: 5.643 
+# Durchschnittsinteresse Programmieren: 5.643
 
-##Auf einer Skala von 1 (sehr gering) bis 7 (sehr hoch) wurden die Variablen "Interesse an Mathematik" und "Interesse am Programmieren"
-##angegeben und der Durchschnitt berechnet. 
-##Bei den Studierenden mit Mathe-LK ist das Interesse an der Mathematik geringfügig höher als an das Interesse am Programmieren.
-##Bei der Studierenden ohne Mathe-LK ist das Interess an der Mathematik und das Interesse am Programmieren gleich.
+## Auf einer Skala von 1 (sehr gering) bis 7 (sehr hoch) wurden die Variablen "Interesse an Mathematik" und "Interesse am Programmieren"
+## angegeben und der Durchschnitt berechnet. 
+## Bei den Studierenden mit Mathe-LK ist das Interesse an der Mathematik (ca. 5.25), geringfügig geringer, 
+## als an das Interesse am Programmieren (ca. 5.6), (Unterschied ca. 7%).
+## Hierbei sind die Interessen der Studenten die den Mathe LK belegt haben, ähnlich mit den allgemeinen Durchschnitten.
+## Bei den Studierenden ohne Mathe-LK ist das Interesse an der Mathematik und das Interesse am Programmieren gleich (ca. 5.65).
+## Außerdem ist hier das Interesse an Mathematik überdurchschnittlich hoch das es ca. 0.3 Punkte über dem durchschnittlichen Mathe-Interesse liegt.
 
 ##
 
